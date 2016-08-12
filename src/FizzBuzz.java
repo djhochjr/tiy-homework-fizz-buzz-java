0063f51a66927d0395b5cd5f5ea26752bb771de1
dave hochstrasser <davehochstrasser@daves-MacBook-Pro.local>
2016-08-12 14:07:29 -0400

    redo

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -7,30 +7,33 @@ public class FizzBuzz {
     public static void doIt(int start, int end) {
 
         for (int x = start; x <= end; x = x + 1) {
-                System.out.print(x);
             //For numbers which are multiples of both three and five print "FizzBuzz".
 
             if ((x % 3 == 0) && (x % 5 == 0)) {
-                System.out.print("FizzBuzz");
+                System.out.println("FizzBuzz");
             }
 
             //For multiples of three print "Fizz" instead of the number.
             else if (x % 3 == 0) {
-                System.out.print("Fizz");
+                System.out.println("Fizz");
 
             }
             //For the multiples of five it print "Buzz".
 
             else if (x % 5 == 0) {
-                System.out.print("Buzz");
+                System.out.println("Buzz");
             }
 
             //For the multiples of five it print "Buzz".
+            else {
+            System.out.println(x);
+            }
 
 
 
 
         }
 
-    }}
+    }
+}
 
